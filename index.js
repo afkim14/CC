c6507c5dfaccb247b0d918028a1819736fda67b5
Andres Kim <andreskim2018@u.northwestern.edu>
2017-12-10 15:39:34 -0600

    guest user

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var users = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('new user', function(username) {
+    //console.log(username);
+    //console.log(socket.id);
   });
 });
 
