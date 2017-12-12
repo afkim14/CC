271358ca3654796f4937065bbf550b22ae2948e7
Junwon Jang <junwonjang2018@u.northwestern.edu>
2017-12-12 02:29:30 -0500

    created socket rooms

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var setup = require('./server/setup');
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
@@ -14,14 +15,17 @@ app.get('/rooms', function(req, res){
   res.send(rooms)
 })
 
+app.use('/', express.static(__dirname + '/public'));
+
 io.on('connection', function(socket){
   socket.on('new user', function(username) {
-    if (username.length <= 10) { // dumb condition for now
+    if (username.length <= 10) { // placeholder condition for now
       var newUser = setup.createUser(username, socket.id);
       users[socket.id] = newUser;
       socket.emit('login response', {user: newUser});
     } else {
-      socket.emit('login response', {user: null});
+      socket.emit('login response', {user: null,
+                                     error: "Failed to login. Make sure username is less than 10 characters"});
     }
   });
 
@@ -29,9 +33,11 @@ io.on('connection', function(socket){
     if (roomTitle.length <= 10) {
       var newRoom = setup.createRoom(roomTitle, users[socket.id]);
       rooms[socket.id] = newRoom;
+      socket.join(roomTitle);
       socket.emit('room response', {room: newRoom});
     } else {
-      socket.emit('room response', {room: null});
+      socket.emit('room response', {room: null,
+                                    error: "Failed to create room. Make sure title is less than 10 characters"});
     }
   });
 
@@ -39,9 +45,58 @@ io.on('connection', function(socket){
     socket.emit('get room response', {rooms: rooms});
   });
 
-  socket.on('quit room', function(res) {
+  socket.on('enter room', function(roomKey) {
+    if (rooms[roomKey]) {
+      rooms[roomKey].players[socket.id] = users[socket.id];
+      for (var socketid in rooms[roomKey].players) {
+        io.to(socketid).emit('room update', {room: rooms[roomKey]});
+      }
+      socket.emit('room response', {room: rooms[roomKey]});
+    } else {
+      socket.emit('room response', {room: null,
+                                    error: "Failed to join room."});
+    }
+  });
+
+  socket.on('open room connection', function(roomTitle) {
+    socket.join(roomTitle);
+  });
+
+  socket.on('quit room', function(data) {
     // TODO: remove player from the room
-    // we should change key from socket id to a room id
+    // we should change key from socket id to a room id ****************!!!!
+    var roomid = data.room.owner.socketid;
+    if (rooms[roomid]) {
+      // if owner, right now transfer ownership but it can be whatever
+      if (rooms[roomid].owner.socketid == socket.id && Object.keys(rooms[roomid].players).length > 1) {
+        for (var socketid in rooms[roomid].players) {
+          if (socketid != socket.id) {
+            rooms[socketid] = rooms[roomid];
+            delete rooms[roomid];
+            roomid = socketid;
+            rooms[roomid].owner = users[socketid];
+          }
+        }
+      }
+
+      delete rooms[roomid].players[socket.id];
+      for (var socketid in rooms[roomid].players) {
+        io.to(socketid).emit('room update', {room: rooms[roomid]});
+      }
+
+      if (Object.keys(rooms[roomid].players).length <= 0) {
+        delete rooms[roomid];
+      }
+    }
+  });
+
+  socket.on('room message', function(data) {
+    var roomId = data.roomId;
+    // data.message is hardcoded right now
+    io.sockets.in(data.roomId).emit('room message', data.message);
+
+    // this holds info about all sockets connected to the room
+    var roster = io.sockets.adapter.rooms[data.roomId];
   });
 
   socket.on('disconnect', function() {
@@ -50,6 +105,7 @@ io.on('connection', function(socket){
     }
   });
 });
+
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
