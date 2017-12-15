5f49c94351b8235e0e7afdbbd9c9af58d5fe2a1a
Andres Kim <andreskim2018@u.northwestern.edu>
2017-12-14 18:13:14 -0600

    window resize capability

--- test/sketch.js
+++ test/sketch.js
@@ -1,6 +1,7 @@
 var canvas;
-var constrainedCanvasWidth = 800;
-var constrainedCanvasHeight = 800;
+var originalCanvasPos;
+var constrainedCanvasWidth = 750;
+var constrainedCanvasHeight = 750;
 var board; // [][]
 var numOfPlayers = 2;
 
@@ -8,18 +9,21 @@ class BoardPiece {
   constructor(row, col) {
     this.position = [row, col];
     this.color = null; // change this later
+    this.button = null; // left and top styles
   }
 }
 
 function setup() {
   canvas = createCanvas(constrainedCanvasWidth, constrainedCanvasHeight);
   canvas.parent('gameCanvas');
+  originalCanvasPos = canvas.position();
   background(100, 100, 100);
   createBoard();
   drawBoard();
 }
 
 function draw() {
+
 }
 
 function createBoard() {
@@ -50,11 +54,10 @@ function createBoard() {
 function drawBoard() {
   var width = board[0].length; // 13
   var height = board.length; // 17
-  var pieceRadius = 30;
   var offsetX = 55; // can't put in array because its mutating
   var offsetY = 44;
-  var startingPosX = constrainedCanvasWidth/2;
-  var startingPosY = 44;
+  var startingPosX = constrainedCanvasWidth/2 - 20;
+  var startingPosY = 10;
   var currentOffset = [startingPosX, startingPosY];
   var gridOffsets = [
     0, offsetX/2, (offsetX/2) * 2, (offsetX/2) * 3,
@@ -69,17 +72,46 @@ function drawBoard() {
     currentOffset[0] = startingPosX - gridOffsets[i];
     for (var j = 0; j < width; j++) {
       if (board[i][j] != null) {
-        push();
-        fill(120,120,120);
-        stroke(120,120,120);
-        strokeWeight(4);
-        ellipse(currentOffset[0], currentOffset[1], pieceRadius);
-        pop();
+        // Have to offset the buttons based on the position of the canvas
+        var newButton = createButton('');
+        newButton.id("emptynode");
+        newButton.position(currentOffset[0], currentOffset[1]);
+        var leftStr = newButton.style("left");
+        var leftFloat = parseFloat(leftStr.substring(0, leftStr.length - 2));
+        var topStr = newButton.style("top");
+        var topFloat = parseFloat(topStr.substring(0, topStr.length - 2));
+        leftFloat += canvas.position().x;
+        topFloat += canvas.position().y;
+        newButton.style("left", leftFloat.toString() + "px");
+        newButton.style("top", topFloat.toString() + "px");
+        board[i][j].button = newButton;
         currentOffset[0] += offsetX;
       }
     }
     currentOffset[1] +=offsetY;
   }
+}
 
-
+function windowResized() {
+  // Resize buttons to fit new window size
+  var offset = [canvas.position().x - originalCanvasPos.x, canvas.position().y - originalCanvasPos.y]; // offset from original canvas size
+  originalCanvasPos.x = canvas.position().x;
+  originalCanvasPos.y = canvas.position().y;
+  var width = board[0].length; // 13
+  var height = board.length; // 17
+  for (var i = 0; i < height; i++) {
+    for (var j = 0; j < width; j++) {
+        if (board[i][j] != null) {
+        var button = board[i][j].button;
+        var leftStr = button.style("left");
+        var leftFloat = parseFloat(leftStr.substring(0, leftStr.length - 2));
+        var topStr = button.style("top");
+        var topFloat = parseFloat(topStr.substring(0, topStr.length - 2));
+        leftFloat += offset[0];
+        topFloat += offset[1];
+        button.style("left", leftFloat.toString() + "px");
+        button.style("top", topFloat.toString() + "px");
+      }
+    }
+  }
 }
