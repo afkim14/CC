cbd025fab05a6b080a64ba52bb17b11e91596532
Junwon Jang <junwonjang2018@u.northwestern.edu>
2017-12-16 20:44:20 -0500

    basic movement

--- test/sketch.js
+++ test/sketch.js
@@ -5,7 +5,7 @@ var constrainedCanvasHeight = 800;
 var board; // [][]
 var boardWidth = 17;
 var boardHeight = 21;
-var numOfPlayers = 6; // this will be sent by the server later (2, 3, 4, 6 implemented)
+var numOfPlayers = 2; // this will be sent by the server later (2, 3, 4, 6 implemented)
 var Color = {
     RED : "#EA5C6A",
     BLUE : "#6DC4E2",
@@ -17,17 +17,92 @@ var Color = {
     CLOSED: "#646464",
 }
 var myColor = Color.RED; // this will also be sent by the server
-var currSelectedPiece; // when user presses a piece
 var tempNeighborColors;
 var currNeighbors;
 
 class BoardPiece {
-  constructor(row, col) {
+  constructor(row, col, team) {
     this.position = {x: row, y: col};
-    this.color = null; // change this later
-    this.button = null; // left and top styles
+    this.selected = false;
+    this.team = team;
+  }
+
+  getValidMoves(pos) {
+    var validMoves = [];
+    var neighborPositions = [];
+    var neighbors = [];
+    var rows = [0, 0, -1, -1, 1, 1];
+    var cols = [-1, 1, -1, 0, -1, 0];
+
+    for (var i=0; i<6; i++) {
+      if (this.position.x % 2 == 0) {
+        neighborPositions.push({x: pos.x + rows[i], y: pos.y + cols[i]});
+      } else {
+        neighborPositions.push({x: pos.x + rows[i], y: pos.y - cols[i]});
+      }
+    }
+
+    var farRows = [0, 0, -2, -2, 2, 2];
+    var farCols = [-2, 2, -1, 1, -1, 1];
+
+    var allNeighbors = neighborPositions.concat();
+    for (var i = 0; i < neighborPositions.length; i++) {
+      if (board[neighborPositions[i].x][neighborPositions[i].y]) {
+        if (board[neighborPositions[i].x][neighborPositions[i].y].boardPiece) {
+          if (this.position.x % 2 == 0) {
+            allNeighbors.push({x: pos.x + farRows[i], y: pos.y + farCols[i]});
+          } else {
+            allNeighbors.push({x: pos.x + farRows[i], y: pos.y - farCols[i]});
+          }
+        }  
+      }
+    }
+
+
+    for (var i = 0; i < allNeighbors.length; i++) {
+      if (board[allNeighbors[i].x][allNeighbors[i].y]) {
+        if (board[allNeighbors[i].x][allNeighbors[i].y].color == Color.EMPTY) {
+          board[allNeighbors[i].x][allNeighbors[i].y].button.mousePressed(destinationSpotSelected.bind(this, {x: allNeighbors[i].x, y: allNeighbors[i].y}));
+          neighbors.push(board[allNeighbors[i].x][allNeighbors[i].y]);
+        }
+      }
+    }
+    console.log(neighbors);
+    return neighbors;
+  }
+
+  movePiece(newPosition) {
+    this.position = newPosition;
   }
 }
+
+class BoardLocation {
+  constructor(row, col) {
+    this.position = {x: row, y: col};
+    this.boardPiece = null;
+    this.color = null;
+    this.button = null;
+  }
+
+  setColor() {
+    this.color = this.boardPiece ? this.boardPiece.team : Color.EMPTY;
+    this.setButtonColor();
+  }
+
+  setButtonColor() {
+    if (this.button) {
+      if (this.boardPiece) {
+        this.button.style("background-color", this.boardPiece.team);
+        this.button.style("border-color", this.boardPiece.team);
+      } else {
+        this.button.style("background-color", Color.EMPTY);
+        this.button.style("border-color", Color.EMPTY);
+      }
+    }
+  }
+}
+
+var currSelectedPiece = new BoardPiece(-1, -1, Color.CLOSED); // when user presses a piece
 
 function setup() {
   canvas = createCanvas(constrainedCanvasWidth, constrainedCanvasHeight);
@@ -40,6 +115,72 @@ function setup() {
 
 function draw() {
 
+}
+
+function positionSelected(pos) {
+  resetNeighbors();
+  currSelectedPiece = board[pos.x][pos.y].boardPiece;
+  currNeighbors = currSelectedPiece.getValidMoves(pos);
+  showValidMoves(currNeighbors);
+}
+
+function showValidMoves(validMoves) {
+  if (validMoves) {
+    for (var i = 0; i < validMoves.length; i++) {
+      validMoves[i].button.style("background-color", Color.YELLOW);
+    }
+  }
+}
+
+function destinationSpotSelected(pos) {
+  // TODO: if valid
+  // reset original position
+  var prevPosition = currSelectedPiece.position;
+  board[prevPosition.x][prevPosition.y].boardPiece = null;
+  board[prevPosition.x][prevPosition.y].setColor();
+  resetNeighbors();
+
+  // move piece to new spot
+  currSelectedPiece.movePiece(pos);
+  board[pos.x][pos.y].button.mousePressed(false);
+  board[pos.x][pos.y].boardPiece = currSelectedPiece;
+  board[pos.x][pos.y].setColor();
+  board[pos.x][pos.y].button.mousePressed(positionSelected.bind(this, pos));
+}
+
+function resetNeighbors() {
+  // resest neighbor colors
+  if (currNeighbors) {
+    for (var i = 0; i < currNeighbors.length; i++) {
+      currNeighbors[i].setColor();
+    }
+  }
+}
+
+function changeButtonPosition(button, offsetX, offsetY) {
+  var leftStr = button.style("left");
+  var leftFloat = parseFloat(leftStr.substring(0, leftStr.length - 2));
+  var topStr = button.style("top");
+  var topFloat = parseFloat(topStr.substring(0, topStr.length - 2));
+  leftFloat += offsetX;
+  topFloat += offsetY;
+  button.style("left", leftFloat.toString() + "px");
+  button.style("top", topFloat.toString() + "px");
+  return button;
+}
+
+function windowResized() {
+  // Resize buttons to fit new window size
+  var offset = [canvas.position().x - originalCanvasPos.x, canvas.position().y - originalCanvasPos.y]; // offset from original canvas size
+  originalCanvasPos.x = canvas.position().x;
+  originalCanvasPos.y = canvas.position().y;
+  for (var i = 0; i < boardHeight; i++) {
+    for (var j = 0; j < boardWidth; j++) {
+      if (board[i][j] != null) {
+        changeButtonPosition(board[i][j].button, offset[0], offset[1]);
+      }
+    }
+  }
 }
 
 function createBoard() {
@@ -61,52 +202,37 @@ function createBoard() {
     var rowOffsetY = rowOffsets[i];
     for (var j = 0; j < actualWidth; j++) {
       if (j >= rowOffsetY && j < rowOffsetY + currRowWidth) {
-        tempBoard[i][j] = new BoardPiece(i+2, j+2);
+        tempBoard[i][j] = new BoardLocation(i+2, j+2);
         if (i < 4) {
           if (numOfPlayers != 4) {
-            tempBoard[i][j].color = Color.RED;
-          } else {
-            tempBoard[i][j].color = Color.CLOSED;
+            tempBoard[i][j].boardPiece = new BoardPiece(i+2, j+2, Color.RED);
           }
         } else if (i < 8) {
           if ((i == 4 && j < 4) || (i == 5 && j < 3) || (i == 6 && j < 3) || (i == 7 && j < 2)) {
             if (numOfPlayers == 4 || numOfPlayers == 6) {
-              tempBoard[i][j].color = Color.BLUE;
-            } else {
-              tempBoard[i][j].color = Color.CLOSED;
+              tempBoard[i][j].boardPiece = new BoardPiece(i, j, Color.BLUE);
             }
           } else if ((i == 4 && j > 8) || (i == 5 && j > 8) || (i == 6 && j > 9) || (i == 7 && j > 9)) {
             if (numOfPlayers == 4 || numOfPlayers == 6) {
-              tempBoard[i][j].color = Color.GREEN;
-            } else {
-              tempBoard[i][j].color = Color.CLOSED;
+              tempBoard[i][j].boardPiece = new BoardPiece(i, j, Color.GREEN);
             }
-          } else {
-            tempBoard[i][j].color = Color.EMPTY;
           }
         } else if (i < 13) {
           if ((i == 9 && j < 2) || (i == 10 && j < 3) || (i == 11 && j < 3) || (i == 12 && j < 4)) {
             if (numOfPlayers != 2) {
-              tempBoard[i][j].color = Color.YELLOW;
-            } else {
-              tempBoard[i][j].color = Color.CLOSED;
-            }
+              tempBoard[i][j].boardPiece = new BoardPiece(i, j, Color.YELLOW);
+            } 
           } else if ((i == 9 && j > 9) || (i == 10 && j > 9) || (i == 11 && j > 8) || (i == 12 && j > 8)) {
             if (numOfPlayers != 2) {
-              tempBoard[i][j].color = Color.BLACK;
-            } else {
-              tempBoard[i][j].color = Color.CLOSED;
-            }
-          } else {
-            tempBoard[i][j].color = Color.EMPTY;
+              tempBoard[i][j].boardPiece = new BoardPiece(i, j, Color.BLACK);
+            } 
           }
         } else {
           if (numOfPlayers == 2 || numOfPlayers == 6) {
-            tempBoard[i][j].color = Color.WHITE;
-          } else {
-            tempBoard[i][j].color = Color.CLOSED;
+            tempBoard[i][j].boardPiece = new BoardPiece(i, j, Color.WHITE);
           }
         }
+        tempBoard[i][j].setColor();
       } else {
         tempBoard[i][j] = null;
       }
@@ -156,10 +282,7 @@ function drawBoard() {
         newButton.id("piece");
         newButton.style("background-color", board[indexOffsetX][indexOffsetY].color);
         newButton.style("border-color", board[indexOffsetX][indexOffsetY].color);
-        if (board[indexOffsetX][indexOffsetY].color == myColor) {
-          // function that is called when clicking one of the nodes
-          newButton.mousePressed(myPieceSelected.bind(this, {x: indexOffsetX, y: indexOffsetY}));
-        }
+        newButton.mousePressed(positionSelected.bind(this, {x: indexOffsetX, y: indexOffsetY}));
         newButton.position(currentOffset[0], currentOffset[1]);
         changeButtonPosition(newButton, canvas.position().x, canvas.position().y);
         board[indexOffsetX][indexOffsetY].button = newButton;
@@ -167,110 +290,5 @@ function drawBoard() {
       }
     }
     currentOffset[1] +=offsetY;
-  }
-}
-
-function myPieceSelected(pos) {
-  if (currSelectedPiece) {
-    resetCurrPieceAndNeighors(myColor);
-  }
-
-  currSelectedPiece = board[pos.x][pos.y];
-  board[pos.x][pos.y].button.style("background-color", Color.EMPTY);
-
-  // activate all possible neigbhors
-  currNeighbors = getNeighborPieces(currSelectedPiece);
-  tempNeighborColors = [];
-  for (var i = 0; i < currNeighbors.length; i++) {
-    tempNeighborColors.push({ background: currNeighbors[i].button.style("background-color"),
-                              border: currNeighbors[i].button.style("border-color") });
-  }
-  showNeighborPieces(currNeighbors);
-}
-
-function getNeighborPieces(p) {
-  var neighbors = [];
-  var neighborPositions;
-  if (p.position.x % 2  == 0) { // even row
-    var neighborPositions = [ {x: p.position.x, y: p.position.y-1},
-                              {x: p.position.x, y: p.position.y+1},
-                              {x: p.position.x-1, y: p.position.y-1},
-                              {x: p.position.x-1, y: p.position.y},
-                              {x: p.position.x+1, y: p.position.y-1},
-                              {x: p.position.x+1, y: p.position.y},
-                            ];
-  } else {                     // odd row
-    var neighborPositions = [ {x: p.position.x, y: p.position.y-1},
-                              {x: p.position.x, y: p.position.y+1},
-                              {x: p.position.x-1, y: p.position.y+1},
-                              {x: p.position.x-1, y: p.position.y},
-                              {x: p.position.x+1, y: p.position.y+1},
-                              {x: p.position.x+1, y: p.position.y},
-                            ];
-  }
-
-  for (var i = 0; i < neighborPositions.length; i++) {
-    if (board[neighborPositions[i].x][neighborPositions[i].y]) {
-      if (board[neighborPositions[i].x][neighborPositions[i].y].color == Color.EMPTY) {
-        board[neighborPositions[i].x][neighborPositions[i].y].button.mousePressed(destinationSpotSelected.bind(this, {x: neighborPositions[i].x, y: neighborPositions[i].y}));
-        neighbors.push(board[neighborPositions[i].x][neighborPositions[i].y]);
-      }
-    }
-  }
-
-  return neighbors;
-}
-
-function showNeighborPieces(neighbors) {
-  for (var i = 0; i < neighbors.length; i++) {
-    neighbors[i].button.style("background-color", myColor);
-    neighbors[i].button.style("border-color", "#000000");
-  }
-}
-
-function resetCurrPieceAndNeighors(currPieceColor) {
-  currSelectedPiece.color = currPieceColor;
-  currSelectedPiece.button.style("background-color", currPieceColor);
-  currSelectedPiece.button.style("border-color", currPieceColor);
-  currSelectedPiece = null;
-  for (var i = 0; i < currNeighbors.length; i++) {
-    // reset neighbors
-    currNeighbors[i].button.style("background-color", tempNeighborColors[i].background);
-    currNeighbors[i].button.style("border-color", tempNeighborColors[i].border);
-  }
-}
-
-function destinationSpotSelected(pos) {
-  // TODO: if valid
-  currSelectedPiece.button.mousePressed(false);
-  resetCurrPieceAndNeighors(Color.EMPTY);
-  board[pos.x][pos.y].button.style("background-color", myColor);
-  board[pos.x][pos.y].button.style("border-color", myColor);
-  board[pos.x][pos.y].button.mousePressed(myPieceSelected.bind(this, pos));
-}
-
-function changeButtonPosition(button, offsetX, offsetY) {
-  var leftStr = button.style("left");
-  var leftFloat = parseFloat(leftStr.substring(0, leftStr.length - 2));
-  var topStr = button.style("top");
-  var topFloat = parseFloat(topStr.substring(0, topStr.length - 2));
-  leftFloat += offsetX;
-  topFloat += offsetY;
-  button.style("left", leftFloat.toString() + "px");
-  button.style("top", topFloat.toString() + "px");
-  return button;
-}
-
-function windowResized() {
-  // Resize buttons to fit new window size
-  var offset = [canvas.position().x - originalCanvasPos.x, canvas.position().y - originalCanvasPos.y]; // offset from original canvas size
-  originalCanvasPos.x = canvas.position().x;
-  originalCanvasPos.y = canvas.position().y;
-  for (var i = 0; i < boardHeight; i++) {
-    for (var j = 0; j < boardWidth; j++) {
-      if (board[i][j] != null) {
-        changeButtonPosition(board[i][j].button, offset[0], offset[1]);
-      }
-    }
   }
 }
